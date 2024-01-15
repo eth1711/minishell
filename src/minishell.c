0c7152c902d9b8b420de8b43759c2d9a17e9532e
ghostyghast <antoinethebestgamer@gmail.com>
2024-01-15 20:13:22 +0800

    exceve now works

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amaligno <antoinemalignon@yahoo.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 18:20:50 by amaligno          #+#    #+#             */
-/*   Updated: 2023/10/27 15:44:31 by amaligno         ###   ########.fr       */
+/*   Updated: 2024/01/15 19:43:02 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,26 @@
 void	parser(char *line, char **envp)
 {
 	char	**tokens;
+	char	*tmp;
 	int		id;
 
 	(void)envp;
 	(void)line;
 	tokens = ft_split(line, ' ');
+	tmp = tokens[0];
+	tokens[0] = ft_strjoin("/usr/bin/", tokens[0]);
 	id = fork();
 	if (!id)
 	{
 		if (execve(tokens[0], tokens, NULL) == -1)
 		{
-			printf("minish: command not found %s\n", tokens[0]);
+			printf("minish: command not found %s\n", tmp);
 			exit(0);
 		}
 	}
 	else
 		wait(&id);
+	free (tmp);
 }
 
 int	main(int argc, char **argv, char **envp)
