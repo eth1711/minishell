a0b10a7929993df2e96249cd84e9a8111a65cc61
eth1711 <ethanlimwl@gmail.com>
2024-04-02 19:04:42 +0800

    done ft_echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,26 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/30 18:11:06 by amaligno          #+#    #+#             */
-/*   Updated: 2024/04/02 18:01:30 by etlim            ###   ########.fr       */
+/*   Created: 2024/03/26 17:38:27 by etlim             #+#    #+#             */
+/*   Updated: 2024/04/02 18:00:45 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cd_cmd(char	**args_array)
+void	ft_echo(char **args_array)
 {
-	if (++(*args_array))
+	int	i;
+	int	flag;
+
+	i = 1;
+	flag = 0;
+	if (ft_strncmp(args_array[i], "-n", 2) == 0)
 	{
-		if (**args_array)
-			chdir(*args_array);
+		flag = 1;
+		i++;
 	}
-	else
+	while (args_array[i])
 	{
-		chdir("/home");
+		ft_putstr_fd(args_array[i], 1);
+		i++;
 	}
+	if (flag == 0)
+		ft_putstr_fd("\n", 1);
 }
