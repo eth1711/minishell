ebb1ed5e5abbac5420ab2f7450759aa6dc7cc6aa
ghostyghast <antoinethebestgamer@gmail.com>
2024-04-03 17:09:52 +0800

    added back exec.c code, commented out unimplemented functios in exec_utils

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -3,11 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
+/*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 17:31:04 by etlim             #+#    #+#             */
-/*   Updated: 2024/04/02 19:03:23 by etlim            ###   ########.fr       */
+/*   Updated: 2024/04/03 17:08:03 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	exec_redir(t_redircmd *redir, t_env *envp)
+{
+	
+}
+
+void	exec_execcmd(t_execcmd *exec, t_env *envp)
+{
+	int		pid;
+	int		ret;
+	char	**envp_array;
+
+	if (is_builtin(exec->args_array, envp))
+		return ;
+	pid = fork();
+	if (!pid)
+	{
+		ret = execve();
+		exit(ret);
+	}
+	wait()
+			
+}
+
+void	exec_pipe(t_pipecmd *pipe, t_env *envp)
+{
+	
+}
+
+void	exec(t_cmd *head, t_env *envp)
+{
+	if (head->type == PIPE)
+		exec_pipe((t_pipecmd *)head, envp);
+	else if (head->type == EXEC)
+		exec_cmd((t_execcmd *)head, envp);
+	else if (head->type == REDIR)	
+		exec_redir((t_redircmd *)head, envp);
+}
--- src/utils/exec_utils.c
+++ src/utils/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 15:29:07 by amaligno          #+#    #+#             */
-/*   Updated: 2024/04/02 17:23:43 by amaligno         ###   ########.fr       */
+/*   Updated: 2024/04/03 17:07:53 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 int	is_builtin(char **args, t_env *envp)
 {
+	(void)envp;
 	if (ft_strcmp(args[0], "echo"))
-		return (ft_echo(args), 1);
+		return (/*ft_echo(args),*/ 1);
 	if (ft_strcmp(args[0], "cd"))
-		return (ft_cd(args), 1);
+		return (/*ft_cd(args),*/ 1);
 	if (ft_strcmp(args[0], "export"))
-		return (ft_export(args, envp), 1);
+		return (/*ft_export(args, envp),*/ 1);
 	if (ft_strcmp(args[0], "pwd"))
-		return (ft_pwd(args, envp), 1);
+		return (/*ft_pwd(args, envp),*/ 1);
 	if (ft_strcmp(args[0], "unset"))
-		return (ft_unset(args, envp), 1);
+		return (/*ft_unset(args, envp),*/ 1);
 	if (ft_strcmp(args[0], "exit"))
-		return (ft_exit(args, envp), 1);
+		return (/*ft_exit(args, envp),*/ 1);
 	return (0);
 }
