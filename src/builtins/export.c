2c48274ce6bfa535a7304541555f889c6d200563
eth1711 <ethanlimwl@gmail.com>
2024-05-31 16:13:41 +0800

    move from laptop to imac

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,12 +6,17 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 14:33:15 by etlim             #+#    #+#             */
-/*   Updated: 2024/05/29 18:56:23 by etlim            ###   ########.fr       */
+/*   Updated: 2024/05/30 17:05:40 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//check the string that is being inputed, for example
+//it takes in export USER=blahblahblah
+//this function should check the before the equal sign (USER=)
+//and check whether it is an alphabet or a permissible character(underscore only)
+//it returns a char pointer back to ft_export
 char    *check_env(char *args, t_env *envp)
 {
     int i;
@@ -25,14 +30,17 @@ char    *check_env(char *args, t_env *envp)
         {
             if (!ft_strchr(args[i], '='))
             {
-                free(agrs[i]);
+                free(args[i]);
                 args[i] = ft_strdup(" ");
-            }   
+            }
         }
     }
     return(args);
     
 }
+
+//the export function takes a string from the terminal, checks it
+// and throws it into the env if conditions/checks are ok
 void	ft_export(char **args_array, t_env *envp)
 {
     if(!check_env(*args_array, envp))
