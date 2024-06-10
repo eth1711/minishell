7ec39351d5545000551f086cf593872da9cff7c2
Ethan Lim <etlim@u81z01s01.42kl.edu.my>
2024-06-10 20:29:29 +0800

    fix : added some stuff to ft_cd, bug : ft_export need to change so that values go into key and values instead of one whole string

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,15 +6,33 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 18:11:06 by amaligno          #+#    #+#             */
-/*   Updated: 2024/06/04 19:58:30 by etlim            ###   ########.fr       */
+/*   Updated: 2024/06/10 20:22:24 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	upd_old_pwd(char *args, t_env *envp)
+{
+	
+}
+
+void	upd_pwd(char *args, t_env *envp)
+{
+	while (*args)
+	{
+		while (envp->next && ft_strncmp(envp->key, "PWD=", 4))
+        	envp = envp->next;
+		if(!ft_strncmp(env->key, "PWD=", 4))	
+		
+	}
+}
+
+
 void	ft_cd(char	*args_array)
 {
 	if (!args_array)
 		return (NULL);
+	chdir(args_array);
 	
 }
