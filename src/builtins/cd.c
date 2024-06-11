fa836b66c4e61e90132cc30a3c6950ceb54762e3
Ethan Lim <etlim@u81z01s05.42kl.edu.my>
2024-06-11 22:06:26 +0800

    fix: updated more stuff to ft_cd, update env function

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,33 +6,38 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 18:11:06 by amaligno          #+#    #+#             */
-/*   Updated: 2024/06/10 20:22:24 by etlim            ###   ########.fr       */
+/*   Updated: 2024/06/11 18:49:23 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	upd_old_pwd(char *args, t_env *envp)
+void	upd_env(char *args, char *key, t_env *envp)
 {
-	
+	int	pwd;
+
+	pwd = NULL;
+	while (envp->next && !ft_strcmp(envp->key, key))
+    	envp = envp->next;
+	if (ft_strcmp(env->key, "OLD_PWD") == 0)
+		pwd = ((ft_strlen(args) - ft_strlen("OLD_PWD=")) - 2)
+		free(envp->value);
+		while(args[++pwd])
+			envp->prevvalue = args[pwd];
+	else if (ft_strcmp(env->key, "PWD") == 0)
+		pwd = ((ft_strlen(args) - ft_strlen("PWD=")) - 2)
+		free(envp->value);
+		while(args[++pwd])
+			envp->value = args[pwd];
 }
 
-void	upd_pwd(char *args, t_env *envp)
-{
-	while (*args)
-	{
-		while (envp->next && ft_strncmp(envp->key, "PWD=", 4))
-        	envp = envp->next;
-		if(!ft_strncmp(env->key, "PWD=", 4))	
-		
-	}
-}
 
-
-void	ft_cd(char	*args_array)
+void	ft_cd(char	*args_array, t_env *envp)
 {
 	if (!args_array)
 		return (NULL);
 	chdir(args_array);
+	upd_env(args_array, "PWD", envp);
+	upd_env(args_array, "OLD_PWD", envp);
 	
 }
