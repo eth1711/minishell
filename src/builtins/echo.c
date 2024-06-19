fbca2dadd99d3074cbd3a583647085b342ea0e4d
ghostyghast <antoinethebestgamer@gmail.com>
2024-06-19 16:32:48 +0800

    feat: ft_echo done

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 17:38:27 by etlim             #+#    #+#             */
-/*   Updated: 2024/05/28 16:55:25 by amaligno         ###   ########.fr       */
+/*   Updated: 2024/06/19 15:35:17 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,28 @@
 
 void	ft_echo(char **args_array)
 {
-	int	i;
 	int	flag;
+    int i;
 
-	i = 1;
 	flag = 0;
-	if (args_array[i] && ft_strncmp(args_array[i], "-n", 2) == 0)
+    i = 2;
+    args_array++;
+    if (args_array && (*args_array)[0] == '-' && (*args_array)[1] == 'n')
+    {
+        while ((*args_array)[i] == 'n')
+            i++;
+        if (!(*args_array)[i])
+        {
+            flag = 1;
+            args_array++;
+        }
+    }
+	while (args_array && *args_array && **args_array)
 	{
-		flag = 1;
-		i++;
-	}
-	while (args_array[i])
-	{
-		ft_putstr_fd(args_array[i], 1);
-		ft_putchar_fd(' ', 1);
-		i++;
+        ft_putstr_fd(*args_array, STDOUT_FILENO);
+        args_array++;
+        if (args_array && *args_array)
+            ft_putchar_fd(' ', STDOUT_FILENO);
 	}
 	if (flag == 0)
 		ft_putstr_fd("\n", 1);
